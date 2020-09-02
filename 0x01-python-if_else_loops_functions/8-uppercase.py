fad3027b5d1c7d8bd06f960e30d455530ca07425
ElianaGomez2020 <elianagomez.s@outlook.com>
2020-09-02 01:06:53 +0000

    task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+def uppercase(str):
+    for a in str:
+        ch = ord(a)
+        if ch >= 97 and ch <= 122:
+            ch -= 32
+        print('{:c}'.format(ch), end='')
+    print('')
