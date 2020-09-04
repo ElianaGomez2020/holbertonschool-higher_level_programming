ce031de5f6c259784f0649b28296ed2f003cefb9
ElianaGomez2020 <elianagomez.s@outlook.com>
2020-09-04 00:22:10 +0000

    tasks

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    arg = len(argv)
-    if arg == 0:
-        print ('0')
-    else:
-        sum = 0
-        for i in range(1, arg):
-            sum += int(argv[i])
-        print("{}".format(sum))
+    sum = 0
+    for i in range(1, len(argv)):
+        sum += int(argv[i])
+    print("{}".format(sum))
