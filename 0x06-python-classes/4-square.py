e82449b781c4e1b890a076872df2d0131516640c
ElianaGomez2020 <elianagomez.s@outlook.com>
2020-09-23 00:00:43 +0000

    tasks

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Square class a private instance attribute init in 0"""
+
+
+class Square:
+    """Attribute size"""
+    def __init__(self, size=0):
+        """
+        Init  a square
+
+        Args:
+            size(int): size of square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """ Setter to size"""
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size ** 2
