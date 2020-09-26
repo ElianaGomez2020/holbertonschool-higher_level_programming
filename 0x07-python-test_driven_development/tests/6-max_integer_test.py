cff9538dc3b35c024334048eb000f51769dc2379
ElianaGomez2020 <elianagomez.s@outlook.com>
2020-09-26 05:14:03 +0000

    tasks

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+
+Unitests for max_integer 
+"""
+
+
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+	"""[summary]
+
+	Args:
+		unittest ([type]): [description]
+	"""
+	def test_empty_list(self)
+		""" Number list cases """
+		self.assertEqual(max_integer([1, 50, 2]), 50)
+    	self.assertEqual(max_integer([-5, -10, -9]), -5)
+    	self.assertEqual(max_integer([1]), 1)
+     	self.assertEqual(max_integer([1, 1, 1, 1]), 1)
+        self.assertEqual(max_integer([2, 4] * 3), 4)
+	
+	def test_empty(self)
+		"""Empty lists """
+		self.assertEqual(max_integer([]), None)
+	
+	def test_not_list(self)
+		"""Not number list"""
+		self.assertEqual(max_integer(['a', 'b', 'c']), a)
+		self.assertEqual(max_integer(('a', 'b', 'c')), a)
