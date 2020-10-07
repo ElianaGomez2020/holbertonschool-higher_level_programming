83fa1b67becb77946531a167e4d5d54a00029b8c
ElianaGomez2020 <elianagomez.s@outlook.com>
2020-10-07 22:38:32 +0000

    tasks

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """[summary] """
-    with open(filename, 'r', 'utf-8') as my_file:
+    with open(filename, 'r', encoding='utf-8') as my_file:
         print(my_file.read(), end='')
