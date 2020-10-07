a211a34b9fd63fb75b9381b6a9c02beac184cd65
ElianaGomez2020 <elianagomez.s@outlook.com>
2020-10-07 02:05:58 +0000

    tasks

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -6,10 +6,9 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """ Rectangle class """
-    def __init__(self, width, height):
-        """ validate if area is implemented """
-        self.integer_validator('width', width)
-        self.integer_validator('heigth', height)
-        self.__width = width
-        self.__height = height
+        """ Rectangle class """
+        def __init__(self, width, height):
+            self.__width = width
+            self.__height = height
+            self.integer_validator('width', width)
+            self.integer_validator('heigth', height)
