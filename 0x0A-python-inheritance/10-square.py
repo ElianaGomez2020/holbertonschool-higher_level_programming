22d46d2a13a11d11cc6d01f71484784e68dcfdad
ElianaGomez2020 <elianagomez.s@outlook.com>
2020-10-07 03:20:40 +0000

    tasks

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" empty class """
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Square class """
+    def __init__(self, size):
+        self.__size = size
+        super().integer_validator('size', size)
+        super().__init__(size, size)
+
+    def area(self):
+        """ return value of area """
+        return self.__size * self.__size
