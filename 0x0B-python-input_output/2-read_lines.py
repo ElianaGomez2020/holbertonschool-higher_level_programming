997e77c490e9d917a3c12297577edb37acd76f51
ElianaGomez2020 <elianagomez.s@outlook.com>
2020-10-07 23:59:28 +0000

    tasks

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -6,7 +6,7 @@ def read_lines(filename="", nb_lines=0):
     """reads n lines of a text file """
     with open(filename, 'r', encoding='utf-8') as my_file:
         if nb_lines <= 0:
-            print(my_file.read())
-            return
-        for i in range(nb_lines):
-            print(my_file.readline())
+            print(my_file.read(), end='')
+        else:
+            for line in range(nb_lines):
+                print(my_file.readline(), end='')
