c235f5eaef15a0a081084e7b8b6c747b9e27e3ab
ElianaGomez2020 <elianagomez.s@outlook.com>
2020-10-08 04:10:57 +0000

    tasks

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Returns the dictionary description with simple data structure"""
+
+
+class Student:
+    """defines a student """
+    def __init__(self, first_name, last_name, age):
+        """Instantiation Public instance attributes """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Studen"""
+        if attrs is not None:
+            new_dic = {}
+            for i in attrs:
+                if i in self.__dict__:
+                    new_dic[i] = self.__dict__
+            return new_dic
+        return self.__dict__
