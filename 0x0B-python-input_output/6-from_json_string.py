03c0aea43f1207c051e681ad4856cf6a20bbc51e
ElianaGomez2020 <elianagomez.s@outlook.com>
2020-10-08 01:18:54 +0000

    tasks

--- 0x0B-python-input_output/6-from_json_string.py
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -6,6 +6,5 @@ import json
 
 
 def from_json_string(my_str):
-    """ returns an object (Python data structure) 
-    represented by a JSON string:"""
+    """ returns an object (Python data structure) """
     return json.loads(my_str)
