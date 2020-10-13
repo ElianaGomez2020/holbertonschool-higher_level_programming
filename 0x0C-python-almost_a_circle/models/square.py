2c4b1871eb3ebd0b26272bb542a4549b32bc6ceb
ElianaGomez2020 <elianagomez.s@outlook.com>
2020-10-13 00:55:46 +0000

    Tasks 14

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -36,3 +36,6 @@ def update(self, *args, **kwargs):
             for key, value in kwargs.items():
                 setattr(self, key, value)
 
+def to_dictionary(self):
+    return {'id': self.id,'size': self.size,'x': self.x,
+            'y': self.y}
