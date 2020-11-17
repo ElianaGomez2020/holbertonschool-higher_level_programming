3fc1e01127b095498f8e285ae3ef88ebd49e6ba0
ElianaGomez2020 <elianagomez.s@outlook.com>
2020-11-17 18:45:22 +0000

    Mysql2

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- lists all privileges of MySQL users user_0d_1 and user _0d_2 on your server
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- list all privileges to user
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
