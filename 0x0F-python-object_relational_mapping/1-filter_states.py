07e8d6044db596b51553ab189bfb2dac557d246e
ElianaGomez2020 <elianagomez.s@outlook.com>
2020-12-13 18:49:07 -0500

    Orm

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,8 @@ if __name__ == '__main__':
                          password=argv[2], db=argv[3])
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states WHERE name LIKE Binary 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE Binary\
+                'N%' ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
