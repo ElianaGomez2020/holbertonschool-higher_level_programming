f5e8cf9f79187ec852d25fdc9bff4c49baaf74f0
ElianaGomez2020 <elianagomez.s@outlook.com>
2020-12-13 19:18:40 -0500

    Orm

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""func"""
+"""contains the class definition of a State"""
+
 
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
 
 class State(Base):
     __tablename__ = 'states'
