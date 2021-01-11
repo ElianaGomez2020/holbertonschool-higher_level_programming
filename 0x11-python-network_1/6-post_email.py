c5cb8452e59c840004c64d37c24dafec6c0aed6d
ElianaGomez2020 <elianagomez.s@outlook.com>
2021-01-11 18:44:12 -0500

    POST an email #1

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""takes in a URL and an email address, sends
+ a POST request to the passed"""
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    value = {"email": sys.argv[2]}
+    r = requests.post(sys.argv[1], data=value)
+    print(r.text)
