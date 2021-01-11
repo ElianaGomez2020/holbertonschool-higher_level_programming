8db93fbb08877fba4cb19c17e031f777c16a6f5f
ElianaGomez2020 <elianagomez.s@outlook.com>
2021-01-11 18:19:40 -0500

    What's my status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,7 +3,7 @@
 import urllib.requests
 
 if __name__ == '__main__':
-    r = request.get('https://intranet.hbtn.io/status')
+    r = requests.get('https://intranet.hbtn.io/status')
     print('Body response:')
     print('\t- type: {}'.format(type(r.text)))
     print('\t- content: {}'.format(r.text))
