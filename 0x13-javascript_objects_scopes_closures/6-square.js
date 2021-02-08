c1976c454d9540c2e184b4dc6755f022f66910df
ElianaGomez2020 <elianagomez.s@outlook.com>
2021-02-08 17:12:34 -0500

    change

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,7 +2,7 @@
 const Square1 = require('./5-square');
 class Square extends Square1 {
   charPrint (c) {
-    if (!c) {
+    if (c === undefined) {
       c = 'X';
     }
     for (let i = 0; i <= this.height; i++) {
