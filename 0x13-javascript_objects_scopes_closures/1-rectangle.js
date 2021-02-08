120a16c2b8937d2ffcac5dd2e5384310c81e4efa
ElianaGomez2020 <elianagomez.s@outlook.com>
2021-02-08 13:53:23 -0500

    resct 0

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-//Write an empty class Rectangle
-class Rectangle{
-    constructor(w,h){
+// Write an empty class Rectangle
+class Rectangle {
+  constructor (w, h) {
     this.width = w;
     this.height = h;
-    }
-
+  }
 }
-module.exports = Rectangle
+module.exports = Rectangle;
